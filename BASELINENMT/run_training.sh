2dbaad9a22a97f289ec4b92f73786e9401310ab1
yaoyiran <yaoyiran@umich.edu>
2020-11-01 21:41:38 +0000

    Update run_training.sh

--- BASELINENMT/run_training.sh
+++ BASELINENMT/run_training.sh
@@ -1,7 +1,3 @@
-rm -r /media/data/translagent2/saved_results
-mkdir /media/data/translagent2/saved_results
-rm log.train
-nvidia-smi
 nohup python ./src/sentence/nmt.py --dataset multi30k --decode_how beam --src en --trg de --task 1 1>log.train &
 
 
