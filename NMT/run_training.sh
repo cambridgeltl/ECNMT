be55bf05a25b47a122eeea9ec7651f16b4f230bb
yaoyiran <yaoyiran@umich.edu>
2020-11-01 23:04:03 +0000

    Update run_training.sh

--- NMT/run_training.sh
+++ NMT/run_training.sh
@@ -1,3 +1,3 @@
-nohup python ./src/sentence/nmt.py --dataset multi30k --decode_how beam --src de --trg en --task 1 1>log.train &
+nohup python ./src/sentence/nmt.py --dataset multi30k --decode_how beam --src de --trg en 1>log.train &
 
 
